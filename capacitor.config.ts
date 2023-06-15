dbe66ae949c917d86839265fe6c56327e67b1582
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-15 03:41:54 +0100

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.themirrorllc.app',
   appName: 'The Mirror',
   webDir: 'dist/themirror',
   bundledWebRuntime: false,
