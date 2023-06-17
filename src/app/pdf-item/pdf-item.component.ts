d9420d63a3321814c109d9a486df46ad7f95c9f8
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-17 01:53:04 +0100

    fixed

--- src/app/pdf-item/pdf-item.component.ts
+++ src/app/pdf-item/pdf-item.component.ts
@@ -1,10 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CourseProgressService } from '../api/services';
+import { BaseComponent } from '../pages/base/base.component';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'app-pdf-item',
   templateUrl: './pdf-item.component.html',
   styleUrls: ['./pdf-item.component.css']
 })
-export class PdfItemComponent {
+export class PdfItemComponent extends BaseComponent {
   @Input() item: any
+  @Input() courseId: any;
+  @Output() done = new EventEmitter();
+
+
+  constructor(data:DataService, router: Router, 
+    private api: CourseProgressService,
+    private notify: NotificationService,
+
+    ){
+    super(data, router)
+  }
+  ngOnInit(): void {
+    super.ngOnInit();
+
+
+  }
+  continue() {
+    this.done.emit(true);
+    this.updateCourseProgress(
+      this.api,
+      this.courseId,
+      this.item.chapterId,
+      this.item.chapterItemId
+    ).subscribe((res) => {
+      this.notify.success('marked as completed');
+    });
+  }
+
 }
