77c3466b7f4cb626b44ac1714c50db274ac2f74f
Emmanuelidus@gmail.com <emmanuelidus@gmail.com>
2023-06-21 19:22:45 +0100

    fix

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -6,32 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
- mycard = [
-  {
-    img: '/assets/portfolio/achievement.png',
-    title: "Achievements",
-    text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
- },
-  {
-    img: '/assets/portfolio/completed.png',
-    title: "Completed Courses",
-    text: 'Your commitment to learning and improving your skills will serve you well in your future endeavors. Well done!'
- },
-  {
-    img: '/assets/portfolio/ongoing.png',
-    title: "On-going Courses",
-    text: 'Keep pushing through the challenges and soak up all the knowledge and experience this course has to offer.'
- },
-  {
-    img: '/assets/portfolio/assesment.png',
-    title: "My Assessment",
-    text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
- },
-  {
-    img: '/assets/portfolio/assesment.png',
-    title: "Sing my song",
-    text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
- },
-
-]
+  mycard = [
+    {
+      img: '/assets/portfolio/achievement.png',
+      title: "My Glories",
+      text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
+   },
+    {
+      img: '/assets/portfolio/completed.png',
+      title: "My Ashes",
+      text: 'Your commitment to learning and improving your skills will serve you well in your future endeavors. Well done!'
+   },
+    {
+      img: '/assets/portfolio/ongoing.png',
+      title: "My Communities",
+      text: 'Keep pushing through the challenges and soak up all the knowledge and experience this course has to offer.'
+   },
+    {
+      img: '/assets/portfolio/assesment.png',
+      title: "My Work in Progress",
+      text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
+   },
+    {
+      img: '/assets/portfolio/singMysong.webp',
+      title: "Singing my song",
+      text: 'Take a moment to celebrate the hard work and dedication that you have shown throughout your learning journey'
+   },
+  
+  ]
 }
